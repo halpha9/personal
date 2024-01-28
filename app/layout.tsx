4a7eb8249fbc5837ae1366ed1cc28e39a7e2055a
Harry Alpha <harryalpha@hotmail.co.uk>
2024-01-28 10:16:28 +0000

    mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
         <link
           rel="apple-touch-icon"
           sizes="180x180"
